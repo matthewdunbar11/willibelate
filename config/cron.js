e7b78f9c271b89d6d392517647f2879cde56e316
Matthew Dunbar <matthewdunbar11@gmail.com>
2016-01-06 20:38:29 -0500

    Cron config

--- config/cron.js
+++ config/cron.js
@@ -1,8 +1,33 @@
+var https = require('https');
+var completed = 0;
 module.exports.cron = {
   myFirstJob: {
     schedule: '* * * * * *',
     onTick: function () {
+    	if (completed == 0) {
+	    	TrafficAlertSubscription.find().then(function(subscriptions) {
+	    		for(subscription in subscriptions) {
+	    			var currentSubscription = subscriptions[subscription];
+					var options = {
+				        host : "maps.googleapis.com",
+				        port : 443,
+				        path : '/maps/api/directions/json?origin=75+9th+Ave+New+York,+NY&destination=MetLife+Stadium+1+MetLife+Stadium+Dr+East+Rutherford,+NJ+07073&key=AIzaSyBoaB8mC8e2kWTChoGT6Wc4uLyasBVNq7U',
+				        method : 'GET'
+				    };
 
+				    var directionsData = '';
+				    console.log('Sending request', currentSubscription);
+				    https.request(options, function(directionsResponse) {
+				        directionsResponse.on('error', function(e){ console.log(e.message); });
+				        directionsResponse.on('data', function(chunk){ directionsData += chunk; console.log(chunk);});
+				        directionsResponse.on('end', function(){
+				        	console.log(directionsData);
+				        });	
+				    });
+	    		}
+	    	});
+	    	completed = 1;
+    	}
     }
   }
 };
